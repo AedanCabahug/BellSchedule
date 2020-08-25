2340bb3575448a69590c2ac16c5b5756e50faa07
Aedan Cabahug <29406873+AedanCabahug@users.noreply.github.com>
2020-08-25 16:20:37 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,352 @@
+/* Customizable Variables */
+let Saturation = 100; // Sets the saturation of the rainbow (Max: 100, Default: 100)
+let Luminance = 60; // Sets the brightness of the rainbow (Max: 100, Default: 60)
+let TickSpeed = 5; // Sets the interval of each tick which affects the rainbow speed (Default: 5)
+let ChromaType = 0; // Sets the type of rainbow animation (Default: 0; 0: Wave, 1: Static, 2: Off)
+let CharOffset = 5; // The color offset of each separate character (Default: 5)
+let TextColor = "#FFFFFF"; // Sets the text color when chroma is disabled (Default: #FFFFFF)
+let Background = ""; // Sets the background image (Default: "")
+let MilitaryTime = false; // Enables 24 hours when set to true (Default: false)
+let EnableSeconds = true; // Enables seconds when set to true (Default: true)
+let DivisionPoints = 360; // Determins how the hue calculations work (Default: 160)
+/* Customizable Variables */
+
+/* Add Schedules Here */
+const Schedules = {};
+Schedules.LateStart = { Periods: [], Days: [1] };
+Schedules.LateStart.Periods.push(1, CreateTimeStamp(09, 30), 222E4);
+Schedules.LateStart.Periods.push(2, CreateTimeStamp(10, 14), 222E4);
+Schedules.LateStart.Periods.push(3, CreateTimeStamp(10, 58), 222E4);
+Schedules.LateStart.Periods.push(4, CreateTimeStamp(11, 42), 222E4);
+Schedules.LateStart.Periods.push(5, CreateTimeStamp(12, 26), 222E4);
+Schedules.LateStart.Periods.push(6, CreateTimeStamp(13, 10), 222E4);
+Schedules.LateStart.Periods.push(7, CreateTimeStamp(13, 54), 222E4);
+Schedules.LateStart.Periods.push(8, CreateTimeStamp(14, 38), 222E4);
+
+Schedules.TueThur = { Periods: [], Days: [2, 4] };
+Schedules.TueThur.Periods.push(1, CreateTimeStamp(08, 00), 45E5);
+Schedules.TueThur.Periods.push(2, CreateTimeStamp(09, 22), 45E5);
+Schedules.TueThur.Periods.push(-1, CreateTimeStamp(10, 37), 18E5);
+Schedules.TueThur.Periods.push(3, CreateTimeStamp(11, 07), 45E5);
+Schedules.TueThur.Periods.push(4, CreateTimeStamp(12, 29), 45E5);
+
+Schedules.WedFri = { Periods: [], Days: [3, 5] };
+Schedules.WedFri.Periods.push(5, CreateTimeStamp(08, 00), 45E5);
+Schedules.WedFri.Periods.push(6, CreateTimeStamp(09, 22), 45E5);
+Schedules.WedFri.Periods.push(-1, CreateTimeStamp(10, 37), 18E5);
+Schedules.WedFri.Periods.push(7, CreateTimeStamp(11, 07), 45E5);
+Schedules.WedFri.Periods.push(8, CreateTimeStamp(12, 29), 45E5);
+/* Add Schedules Here */
+
+let AnimTimer = 0;
+let Timer = 0;
+let CurrentSchedule;
+
+function Pad(X) {
+    return X < 10 ? '0' + X : X + "";
+}
+
+function Hex(X) {
+    if (X.toString(16).length < 2) return '0' + X;
+    return X.toString(16);
+}
+
+function CreateTimeStamp(H, M, S) {
+    return new Date("Tue January 1 2000 " + (H + ":" + M + ":" + (S || 00)));
+}
+
+function Rainbow(Offset) {
+    if (ChromaType == 0) Offset += Timer;
+    if (Offset >= DivisionPoints) Offset -= DivisionPoints;
+
+    let RGB = ConvertHSL(ChromaType ? Math.round((Timer / DivisionPoints) * 100) / 100 : Math.round((Offset / DivisionPoints) * 100) / 100, Saturation / 100, Luminance / 100)
+    return '#' + Hex(RGB[0].toString(16)) + Hex(RGB[1].toString(16)) + Hex(RGB[2].toString(16));
+}
+
+function ConvertHue(P, Q, T){
+    if (T < 0) T += 1;
+    if (T > 1) T -= 1;
+    if (T < 1/6) return P + (Q - P) * 6 * T;
+    if (T < 1/2) return Q;
+    if (T < 2/3) return P + (Q - P) * (2/3 - T) * 6;
+    return P;
+}
+
+function ConvertHSL(H, S, L){
+    let R, G, B;
+
+    if (S == 0) {
+        R = G = B = l;
+    } else {
+        let Q = L < 0.5 ? L * (1 + S) : L + S - L * S;
+        let P = 2 * L - Q;
+        R = ConvertHue(P, Q, H + 1/3);
+        G = ConvertHue(P, Q, H);
+        B = ConvertHue(P, Q, H - 1/3);
+    }
+
+    return [Math.round(R * 255), Math.round(G * 255), Math.round(B * 255)];
+}
+
+function GetPeriod() {
+    let CurrentTime = GetCurrentTime();
+
+    for (let i = 0; i < CurrentSchedule.Periods.length; i++) {
+        if (CurrentTime >= CurrentSchedule.Periods[i].Start && CurrentTime <= CurrentSchedule.Periods[i].End) {
+            return CurrentSchedule.Periods[i];
+        }
+    }
+}
+
+function DetermineSchedule() {
+    for (let i = 0; i < Object.keys(Schedules).length; i++) {
+        if (Schedules[Object.keys(Schedules)[i]].Days.indexOf(new Date().getDay()) >= 0) {
+            CurrentSchedule = Schedules[Object.keys(Schedules)[i]];
+        }
+    }
+}
+
+function NextPeriod() {
+    let CurrentTime = CreateTimeStamp(new Date().getHours(), new Date().getMinutes());
+
+    for (let i = 1; i < CurrentSchedule.Periods.length; i++) {
+        if (CurrentTime <= CurrentSchedule.Periods[i].Start && CurrentTime >= CurrentSchedule.Periods[i - 1].End) {
+            return CurrentSchedule.Periods[i];
+        }
+    }
+}
+
+function GetCurrentTime() {
+    return CreateTimeStamp(new Date().getHours(), new Date().getMinutes(), new Date().getSeconds()).getTime();
+}
+
+setInterval(function () {
+    AnimTimer ++;
+    if (AnimTimer % TickSpeed == 0) Timer ++;
+    if (Timer > DivisionPoints) Timer = 0;
+
+    DetermineSchedule();
+    let ClockElement = document.querySelector(".time-clock");
+    let PeriodElement = document.querySelector(".time-period");
+    let Hours = Pad(new Date().getHours());
+    let PM = new Date().getHours() >= 12;
+
+    if (!MilitaryTime && Hours > 12) Hours -= 12;
+
+    let ClockText = Hours + ":" + Pad(new Date().getMinutes()) + (EnableSeconds ? Pad(":" + Pad(new Date().getSeconds())) : "") + " ";
+    let CurrentPeriod = GetPeriod();
+    let PeriodText = "";
+
+    if (!MilitaryTime) ClockText += PM ? "PM" : "AM";
+
+    if (!CurrentPeriod) {
+        if (!NextPeriod()) {
+            PeriodText = "School Not in Session";
+        } else {
+            let PassingTimeLeft = (NextPeriod().Start - GetCurrentTime) / 6E4;
+            if (TimeLeft >= 60) {
+                let Remainder = TimeLeft - (Math.floor(TimeLeft / 60) * 60);
+                let SkipAppend = false;
+                TimeLeft = Math.floor(TimeLeft / 60) + " Hours " + Remainder + " ";
+                if (Remainder == 0) {
+                    TimeLeft = Math.floor(TimeLeft / 60) + " Hour";
+                    if (CurrentPeriod.Name == -1) {
+                        PeriodText = "Passing Period " + CurrentPeriod.Name + " (" + Math.ceil(TimeLeft) + " Seconds left)";
+                    } else {
+                        PeriodText = "Passing Period " + CurrentPeriod.Name + " (" + Math.ceil(TimeLeft) + " Seconds left)";
+                    }
+                    SkipAppend = true;
+                }
+            }
+            if (!SkipAppend) { 
+                if (TimeLeft < 1) {
+                    let Seconds = (NextPeriod().Start - GetCurrentTime()) / 1E3;
+                    PeriodText = "Passing Period (" + Math.ceil(Seconds) + " Seconds left)";
+                } else PeriodText = "Passing Period (" + Math.ceil(PassingTimeLeft)  + " Minutes left)";
+            }
+        }
+    } else {
+        let TimeLeft = (CurrentPeriod.End - GetCurrentTime()) / 6E4;
+        let SkipAppend = false;
+        if (TimeLeft >= 60) {
+            let Remainder = TimeLeft - (Math.floor(TimeLeft / 60) * 60);
+            TimeLeft = Math.floor(TimeLeft / 60) + " Hours " + Remainder + " ";
+            if (Remainder == 0) {
+                TimeLeft = Math.floor(TimeLeft / 60) + " Hour";
+                if (CurrentPeriod.Name == -1) {
+                    PeriodText = "Break Period " + CurrentPeriod.Name + " (" + Math.ceil(TimeLeft) + " Seconds left)";
+                } else {
+                    PeriodText = "Period " + CurrentPeriod.Name + " (" + Math.ceil(TimeLeft) + " Seconds left)";
+                }
+                SkipAppend = true;
+            }
+        }
+        if (!SkipAppend) {
+            if (CurrentPeriod.Name == -1) {
+                if (TimeLeft < 1) {
+                    let Seconds = (CurrentPeriod.End - GetCurrentTime()) / 1E3;
+                    PeriodText = "Break Period " + CurrentPeriod.Name + " (" + Math.ceil(Seconds) + " Seconds left)";
+                } else PeriodText = "Break Period (" + TimeLeft + " Minutes left)";
+            } else {
+                if (TimeLeft < 1) {
+                    let Seconds = (CurrentPeriod.End - GetCurrentTime()) / 1E3;
+                    PeriodText = "Period " + CurrentPeriod.Name + " (" + Math.ceil(Seconds) + " Seconds left)";
+                } else PeriodText = "Period " + CurrentPeriod.Name + " (" + Math.ceil(TimeLeft) + " Minutes left)";
+            }
+        }
+    }
+
+    if (ClockText != ClockElement.innerHTML) {
+        ClockElement.innerHTML = ClockText;
+
+        let Arr = ClockElement.innerHTML.split("");
+        let SplitArr = [];
+
+        ClockElement.innerHTML = "";
+        for (let i = 0; i < Arr.length; i++) {
+            let ChildElement = document.createElement("span");
+            ChildElement.innerHTML = Arr[i];
+            ClockElement.appendChild(ChildElement);
+        }
+    }
+
+    if (PeriodText != PeriodElement.innerHTML) {
+        PeriodElement.innerHTML = PeriodText;
+
+        let Arr = PeriodElement.innerHTML.split("");
+        let SplitArr = [];
+
+        PeriodElement.innerHTML = "";
+        for (let i = 0; i < Arr.length; i++) {
+            let ChildElement = document.createElement("span");
+            ChildElement.innerHTML = Arr[i];
+            PeriodElement.appendChild(ChildElement);
+        }
+    }
+
+    for (let i = 0; i < ClockElement.children.length; i++) {
+        if (ChromaType == 2) {
+            ClockElement.children[i].style.color = TextColor;
+        } else ClockElement.children[i].style.color = Rainbow(i * CharOffset);
+    }
+
+    for (let i = 0; i < PeriodElement.children.length; i++) {
+        if (ChromaType == 2) {
+            PeriodElement.children[i].style.color = TextColor;
+        } else PeriodElement.children[i].style.color = Rainbow(i * CharOffset);
+    }
+});
+
+function StoreSettings() {
+    if (localStorage) {
+        localStorage.setItem("Settings", JSON.stringify({
+            Saturation: document.querySelector(".color-saturation").value,
+            Luminance: document.querySelector(".color-luminance").value,
+            TickSpeed: 4 - document.querySelector(".color-speed").value,
+            CharOffset: document.querySelector(".color-range").value,
+            TextColor: document.querySelector(".color-input").value,
+            Background: document.querySelector(".background").value,
+            MilitaryTime: document.querySelector(".military-time").checked,
+            EnableSeconds: document.querySelector(".seconds-time").checked,
+        }));
+    }
+}
+
+function LoadSettings() {
+    if (localStorage && localStorage.getItem("Settings")) {
+        let Loaded = JSON.parse(localStorage.getItem("Settings"));
+        Saturation =  Loaded.Saturation; 
+        Luminance = Loaded.Luminance;
+        TickSpeed = Loaded.TickSpeed;
+        CharOffset =  Loaded.CharOffset;
+        TextColor = Loaded.TextColor;
+        Background = Loaded.Background;
+        MilitaryTime = Loaded.MilitaryTime;
+        EnableSeconds = Loaded.EnableSeconds;
+        UpdateSettingsContainer();
+    } else StoreSettings();
+    UpdateClockBackground();
+}
+
+function UpdateClockBackground() {
+    document.body.style.background = "url(" + (Background || "./images/Background.jpg") + ")";
+    document.body.style.backgroundSize = "cover";
+
+    let Img = document.createElement("img");
+    Img.src = Background;
+
+    Img.addEventListener("error", function () {
+        document.body.style.background = "url(./images/Background.jpg)";
+        document.body.style.backgroundSize = "cover";
+    });
+}
+
+function UpdateSettingsContainer() {
+    document.querySelector(".color-saturation").value = Saturation; 
+    document.querySelector(".color-luminance").value = Luminance;
+    document.querySelector(".color-speed").value = TickSpeed + 4;
+    document.querySelector(".color-range").value = CharOffset;
+    document.querySelector(".color-input").value = TextColor;
+    document.querySelector(".background").value = Background;
+    document.querySelector(".military-time").checked = MilitaryTime;
+    document.querySelector(".seconds-time").checked = EnableSeconds;
+
+    if (ChromaType == 0) document.querySelector(".chroma-button").innerHTML = "Chroma Type: Wave";
+    if (ChromaType == 1) document.querySelector(".chroma-button").innerHTML = "Chroma Type: Static";
+    if (ChromaType == 2) document.querySelector(".chroma-button").innerHTML = "Chroma Type: Off";
+
+    if (ChromaType != 0) {
+        document.querySelector(".color-range").disabled = true;
+    } else document.querySelector(".color-range").disabled = false;
+}
+
+window.addEventListener("load", function () {
+    LoadSettings();
+
+    document.querySelector(".settings").addEventListener("click", function () {
+        if (document.querySelector(".settings-container").style.opacity == "1")
+            document.querySelector(".settings-container").style.opacity = "0";
+        else document.querySelector(".settings-container").style.opacity = "1";
+
+        UpdateSettingsContainer();
+    });
+
+    document.querySelector(".submit").addEventListener("click", function () {
+        StoreSettings();
+    });
+
+    document.querySelector(".chroma-button").addEventListener("click", function () {
+        ChromaType ++;
+        if (ChromaType > 2) ChromaType = 0;
+        UpdateSettingsContainer();
+    });
+
+    document.querySelector(".reset").addEventListener("click", function () {
+        Saturation = 100;
+        Luminance = 60;
+        TickSpeed = 2;
+        ChromaType = 0;
+        CharOffset = 5;
+        TextColor = "#FFFFFF";
+        Background = "";
+        MilitaryTime = false;
+        EnableSeconds = true;
+        UpdateSettingsContainer();
+        StoreSettings();
+    });
+
+    setInterval(function () {
+        Saturation = document.querySelector(".color-saturation").value;
+        Luminance = document.querySelector(".color-luminance").value;
+        TickSpeed = 4 - document.querySelector(".color-speed").value;
+        CharOffset = document.querySelector(".color-range").value;
+        TextColor = document.querySelector(".color-input").value;
+        Background = document.querySelector(".background").value;
+        MilitaryTime = document.querySelector(".military-time").checked;
+        EnableSeconds = document.querySelector(".seconds-time").checked;  
+
+        if (document.body.style.background != "url(" + Background + ")") {
+            UpdateClockBackground();
+        }
+    });
+});
